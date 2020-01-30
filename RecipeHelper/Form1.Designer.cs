73013219744fb7fd39f2812b23b142e3bc45cc16
alicia65 <alicia65>
2020-01-30 01:02:09 -0600

    add codes to converter button

--- RecipeHelper/Form1.Designer.cs
+++ RecipeHelper/Form1.Designer.cs
@@ -32,38 +32,44 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.txtCelsius = new System.Windows.Forms.TextBox();
+            this.txtMillilitres = new System.Windows.Forms.TextBox();
+            this.txtFahrenheit = new System.Windows.Forms.TextBox();
+            this.txtCups = new System.Windows.Forms.TextBox();
+            this.btnConvertToFahrenheit = new System.Windows.Forms.Button();
+            this.btnConvertToCups = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(36, 33);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 52);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 17);
+            this.label1.Size = new System.Drawing.Size(64, 18);
             this.label1.TabIndex = 0;
             this.label1.Text = "Celsius";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(247, 38);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(382, 64);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(61, 17);
+            this.label2.Size = new System.Drawing.Size(72, 17);
             this.label2.TabIndex = 1;
             this.label2.Text = "Millilitres";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 228);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(21, 356);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(76, 17);
+            this.label3.Size = new System.Drawing.Size(87, 18);
             this.label3.TabIndex = 2;
             this.label3.Text = "Fahrenheit";
             this.label3.Click += new System.EventHandler(this.label3_Click);
@@ -71,76 +77,90 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(235, 233);
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(382, 364);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(40, 17);
+            this.label4.Size = new System.Drawing.Size(47, 18);
             this.label4.TabIndex = 3;
             this.label4.Text = "Cups";
             // 
-            // textBox1
+            // txtCelsius
             // 
-            this.textBox1.Location = new System.Drawing.Point(105, 33);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 4;
+            this.txtCelsius.Location = new System.Drawing.Point(135, 60);
+            this.txtCelsius.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.txtCelsius.Name = "txtCelsius";
+            this.txtCelsius.Size = new System.Drawing.Size(160, 30);
+            this.txtCelsius.TabIndex = 4;
             // 
-            // textBox2
+            // txtMillilitres
             // 
-            this.textBox2.Location = new System.Drawing.Point(314, 35);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
-            this.textBox2.TabIndex = 5;
+            this.txtMillilitres.AcceptsReturn = true;
+            this.txtMillilitres.Location = new System.Drawing.Point(511, 64);
+            this.txtMillilitres.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.txtMillilitres.Name = "txtMillilitres";
+            this.txtMillilitres.Size = new System.Drawing.Size(160, 30);
+            this.txtMillilitres.TabIndex = 5;
             // 
-            // textBox3
+            // txtFahrenheit
             // 
-            this.textBox3.Location = new System.Drawing.Point(105, 228);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.ReadOnly = true;
-            this.textBox3.Size = new System.Drawing.Size(100, 22);
-            this.textBox3.TabIndex = 6;
-            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            this.txtFahrenheit.Location = new System.Drawing.Point(170, 356);
+            this.txtFahrenheit.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.txtFahrenheit.Name = "txtFahrenheit";
+            this.txtFahrenheit.ReadOnly = true;
+            this.txtFahrenheit.Size = new System.Drawing.Size(160, 30);
+            this.txtFahrenheit.TabIndex = 6;
+            this.txtFahrenheit.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
-            // textBox4
+            // txtCups
             // 
-            this.textBox4.Location = new System.Drawing.Point(314, 233);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.ReadOnly = true;
-            this.textBox4.Size = new System.Drawing.Size(100, 22);
-            this.textBox4.TabIndex = 7;
+            this.txtCups.Location = new System.Drawing.Point(511, 364);
+            this.txtCups.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.txtCups.Name = "txtCups";
+            this.txtCups.ReadOnly = true;
+            this.txtCups.Size = new System.Drawing.Size(160, 30);
+            this.txtCups.TabIndex = 7;
             // 
-            // button1
+            // btnConvertToFahrenheit
             // 
-            this.button1.Location = new System.Drawing.Point(39, 100);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(115, 63);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "Convert to Fahrenheit";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnConvertToFahrenheit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvertToFahrenheit.Location = new System.Drawing.Point(64, 156);
+            this.btnConvertToFahrenheit.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.btnConvertToFahrenheit.Name = "btnConvertToFahrenheit";
+            this.btnConvertToFahrenheit.Size = new System.Drawing.Size(187, 99);
+            this.btnConvertToFahrenheit.TabIndex = 8;
+            this.btnConvertToFahrenheit.Text = "Convert to Fahrenheit";
+            this.btnConvertToFahrenheit.UseVisualStyleBackColor = true;
+            this.btnConvertToFahrenheit.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // btnConvertToCups
             // 
-            this.button2.Location = new System.Drawing.Point(290, 100);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(103, 63);
-            this.button2.TabIndex = 9;
-            this.button2.Text = "Convert to Cups";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnConvertToCups.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvertToCups.Location = new System.Drawing.Point(472, 156);
+            this.btnConvertToCups.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.btnConvertToCups.Name = "btnConvertToCups";
+            this.btnConvertToCups.Size = new System.Drawing.Size(168, 99);
+            this.btnConvertToCups.TabIndex = 9;
+            this.btnConvertToCups.Text = "Convert to Cups";
+            this.btnConvertToCups.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(1300, 702);
+            this.Controls.Add(this.btnConvertToCups);
+            this.Controls.Add(this.btnConvertToFahrenheit);
+            this.Controls.Add(this.txtCups);
+            this.Controls.Add(this.txtFahrenheit);
+            this.Controls.Add(this.txtMillilitres);
+            this.Controls.Add(this.txtCelsius);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.Name = "Form1";
             this.Text = "Recipe Helper";
             this.ResumeLayout(false);
@@ -154,12 +174,12 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtCelsius;
+        private System.Windows.Forms.TextBox txtMillilitres;
+        private System.Windows.Forms.TextBox txtFahrenheit;
+        private System.Windows.Forms.TextBox txtCups;
+        private System.Windows.Forms.Button btnConvertToFahrenheit;
+        private System.Windows.Forms.Button btnConvertToCups;
     }
 }
 
--- RecipeHelper/Form1.cs
+++ RecipeHelper/Form1.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*
+*C# program to Convert Fahrenheit to Cesius
+* and Millilitres to Cups
+*/
+   
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -25,6 +30,12 @@ namespace RecipeHelper
         private void textBox3_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtCelsius.Text = (Int) ((txtFahrenheit.Text - 32) * 5 / 9);
+            txtFahrenheit.Text = 
         }
     }
 }
